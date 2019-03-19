cb493827c5d0210e9b50df411261dee785c3f8c1
Gabriel Rębacz <01121840@pw.edu.pl>
2019-03-19 14:49:30 +0100

    QT1 - init layout

---
+++ Qt Widget - FractalVisualizer/FractalVisualizer/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->typeComboBox->addItem("Mandelbrot");
+    ui->typeComboBox->addItem("Julia");
+    ui->colorsComboBox->addItem("Standard");
+    ui->resolutionComboBox->addItem("1280-768");
+//    this->setFixedSize(QSize(460,400));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}

e15cba4fa434a37631eddb0a8e917c5ce93d4853
grebacz <gabrielrebacz@gmail.com>
2019-03-21 09:03:58 +0100

    werwer

--- Qt Widget - FractalVisualizer/FractalVisualizer/fractalwindow.cpp
+++ Qt Widget - FractalVisualizer/FractalVisualizer/fractalwindow.cpp
@@ -3,9 +3,8 @@
 #include <QGraphicsPixmapItem>
 #include <QGraphicsScene>
 
-FractalWindow::FractalWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::FractalWindow)
+FractalWindow::FractalWindow(QWidget *parent, int width, int height)
+    : QMainWindow(parent), ui(new Ui::FractalWindow) ,width(width), height(height)
 {
     ui->setupUi(this);
     fractalImage = new FractalImage();
@@ -14,6 +13,7 @@ FractalWindow::FractalWindow(QWidget *parent) :
     scene.addItem(new QGraphicsPixmapItem(pixMap));
     ui->graphicsView->setScene(&scene);
 
+    this->setFixedSize(QSize(width+50, height+50));
     ui->graphicsView->show();
 
 }
--- Qt Widget - FractalVisualizer/FractalVisualizer/fractalwindow.h
+++ Qt Widget - FractalVisualizer/FractalVisualizer/fractalwindow.h
@@ -17,13 +17,15 @@ class FractalWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit FractalWindow(QWidget *parent = nullptr);
+    explicit FractalWindow(QWidget *parent = nullptr, int width = 1286, int height = 768);
     ~FractalWindow();
 
 private:
     Ui::FractalWindow* ui;
     QGraphicsScene scene;
+    int width, height;
     FractalImage* fractalImage;
+
 };
 
 #endif // FRACTALWINDOW_H

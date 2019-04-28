c3dbac8341f7918317493a0dac59144a10199120
Gabriel Rębacz <gabrielrebacz@gmail.com>
2019-04-28 23:58:06 +0200

    Win forms app

---
+++ WinForms - Book Editor/MDIApp/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDIApp
+{
+    public class Book
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public Book(long id, string title, string author, DateTime releaseDate, string genre)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+        }
+    }
+}
